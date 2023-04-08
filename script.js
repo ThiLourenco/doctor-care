75267f35096013af7caa6312962a0c11cd143aee
Thigo <thi.it.solutions@gmail.com>
2023-04-08 12:13:14 -0300

    chore(): deploy

--- script.js
+++ script.js
@@ -11,20 +11,17 @@ function onScroll() {
   activateMenuAtCurrentSection(contact)
 }
 
+// handler position scrolling
 function activateMenuAtCurrentSection(section) {
   const targetLine = scrollY + innerHeight / 2
 
-  // verificar se a seção passou da linha
-  // quais dados vou precisar?
   const sectionTop = section.offsetTop
   const sectionHeight = section.offsetHeight
   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
 
-  // verificar se a base está abaixo da linha alvo
   const sectionEndsAt = sectionTop + sectionHeight
   const sectionEndPassedTargetline = sectionEndsAt <= targetLine
 
-  // limites da seção
   const sectionBoundaries =
     sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
 
